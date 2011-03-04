f9f51e97fa11554bc14ee8c34329ddfc66152fdc
Thilina Gunarathne <tgunarat@indiana.edu>
2011-03-04 00:53:42 +0000

    more comments

--- hadoop/fg-hadoop.py
+++ hadoop/fg-hadoop.py
@@ -1,4 +1,18 @@
 #! /usr/bin/python
+"""
+A shell command to dynamically deploy an Apache Hadoop environments on FG.
+
+This command line tool deploys Apache Hadoop in to a FutureGrid resource 
+and executes the given job. Users can specify a directory containing input 
+data, which will get uploaded to the HDFS under the "input" directory. 
+Users can also specify a directory to download the output data (contents 
+of the "output" directory) from HDFS. HADOOP_HOME environment variable, 
+pointing to the Hadoop distribution, needs to be set before running this 
+command.
+"""
+
+__author__ = 'Thilina Gunarathne'
+__version__ = '0.1'
 
 import subprocess
 import time
@@ -16,6 +30,7 @@ import argparse
 #
 
 # TODO : Configure the conf location
+
 def generate_job_script(job_name, hadoop_command, data_input_dir,
                         data_output_dir, walltime, num_nodes, queue,) :
     local_storage_dir = "/tmp/$PBS_JOBID-fg-hadoop"
@@ -45,7 +60,9 @@ def generate_job_script(job_name, hadoop_command, data_input_dir,
 
     masters_file.write("echo starting the cluster  \n")
     masters_file.write("$HADOOP_HOME/bin/start-dfs.sh  \n") #--config $HADOOP_CONF_DIR
-    masters_file.write("$HADOOP_HOME/bin/hadoop dfsadmin -safemode wait \n\n") 
+    masters_file.write("$HADOOP_HOME/bin/hadoop dfsadmin -safemode wait \n") 
+    masters_file.write("sleep 30 \n") # safemode wait seems to be still buggy
+    
     if(data_input_dir):
         masters_file.write("$HADOOP_HOME/bin/hadoop fs -put " 
                            + data_input_dir + " input \n") 
@@ -63,19 +80,19 @@ def generate_job_script(job_name, hadoop_command, data_input_dir,
     masters_file.write("$HADOOP_HOME/bin/stop-dfs.sh" + "\n")
     
     masters_file.close()
+    return masters_file_name
     
     
 def main():        
     parser = argparse.ArgumentParser(description='Run a Hadoop Job in FutureGrid')
-    
     parser.add_argument('jobname', help='Name of the job')
-    parser.add_argument('inputdir', help='Directory containing the input data for the job')
-    parser.add_argument('outputdir', help='Directory to store the output data from the job')
     
     #optional arguments
+    parser.add_argument('-i','--inputdir', help='Directory containing the input data for the job')
+    parser.add_argument('-o','--outputdir', help='Directory to store the output data from the job')
     parser.add_argument('-q', '--queue', help='Queue to submit the job', default="batch")
-    # TODO: add a type to validate walltime
-    parser.add_argument('-w', '--walltime', help='Walltime for the job (00:00:00)', default='00:20:00')    
+    # TODO: add a type function to validate walltime
+    parser.add_argument('-w', '--walltime', help='Walltime for the job (hh:mm:ss)', default='00:20:00')    
     parser.add_argument('-n', '--nodes', help='Number of nodes for the job', default=1, type=int)    
     
     #hadoop command
@@ -92,11 +109,12 @@ def main():
         
     hadoop_cmd = ' '.join(args.hadoopcmd)
     
-    generate_job_script(args.jobname,hadoop_cmd, args.inputdir, args.outputdir
-                        ,args.walltime,args.nodes,args.queue)
-# export HADOOP_LOG_DIR=${HADOOP_HOME}/log
-# export HADOOP_HOME
-# export HADOOP_CONF_DIR 
+    job_script = generate_job_script(args.jobname,hadoop_cmd, 
+        args.inputdir, args.outputdir,args.walltime,args.nodes,args.queue)
+    
+    subprocess.call("qsub " + job_script, shell=True)
 
+    
+# TODO export HADOOP_LOG_DIR=${HADOOP_HOME}/log
 if __name__ == "__main__":
     main()
