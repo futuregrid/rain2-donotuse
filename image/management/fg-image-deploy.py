a98756a5c1ae15eec3c290b5deaadb53b2a3f200
Javi Diaz <javier.diazmontes@gmail.com>
2011-05-11 22:24:00 +0000

    image generation

--- image/management/fg-image-deploy.py
+++ image/management/fg-image-deploy.py
@@ -214,7 +214,7 @@ def main():
             runCmd('sudo rm -f var.tgz torque-2.5.1.tgz')
             runCmd('sudo wget fg-gravel3.futuregrid.iu.edu/torque/torque-2.5.1_minicluster/pbs_mom -O '+tempdir+'/rootimg/etc/init.d/pbs_mom')
             runCmd('sudo chmod +x '+tempdir+'/rootimg/etc/init.d/pbs_mom')
-            runCmd('sudo chroot '+tempdir+'/rootimg/ chkconfig --add pbs_mom')
+            runCmd('sudo chroot '+tempdir+'/rootimg/ /sbin/chkconfig --add pbs_mom')
             
         else:#Later we should be able to chose the cluster where is deployed
             logger.info('Torque for India')    
