110b40e1e8f513109dc0ec47a28c95bd289812da
Gregor von Laszewski <laszewski@gmail.com>
2011-07-29 14:20:37 +0000

    added some comments

--- utils/fgLog.py
+++ utils/fgLog.py
@@ -5,6 +5,13 @@ import logging.handlers
 class fgLog():
 
     def __init__(self, logfile, loglevel, whois, verbose):
+        '''initializes the log file. the parameters are as follows
+        logfile: name of the log file
+        loglevel: setting of the log level 
+        TODO: explain what the loglevels do
+        whois: name associated with the log
+        verbose: if True prints some information to stdout
+        '''
         self._logger = logging.getLogger(whois)
         self._formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         self._logger.setLevel(loglevel)
@@ -18,7 +25,7 @@ class fgLog():
         self._logger.addHandler(handler)
         self._logger.propagate = False
 
-        # This is to print in the stout the same that in the log
+        # This is to print in the stdout the same that in the log
         if(verbose):
             ch = logging.StreamHandler()
             ch.setLevel(loglevel)
@@ -28,19 +35,25 @@ class fgLog():
 
 
     def getLogFile(self):
+        '''returns the log file'''
         return self._logfile
 
     def debug(self, text):
+        '''includes a debug message of "text" into the log file'''
         self._logger.debug(text)
 
     def info(self, text):
+        '''includes an info message of "text" into the log file'''
         self._logger.info(text)
 
     def warning(self, text):
+        '''includes a warning message of "text" into the log file'''
         self._logger.warning(text)
 
     def error(self, text):
+        '''includes an error message of "text" into the log file'''
         self._logger.error(text)
 
     def clear(self):
+        '''removes the log file'''
         os.remove(self._logfile)
