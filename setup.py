2dece52a8d7c9033c4ebf6ff4f36258a1068e41e
Gregor von Laszewski <laszewski@gmail.com>
2011-08-01 14:26:27 +0000

    home of the scripts

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 """ 
 Setup for the futuregrid
 """
-
 from setuptools import setup
-
 
 setup(
     name = 'futuregrid',
@@ -18,13 +16,13 @@ futuregrid is a python library that contains the code related dynamically provis
     url = "http://futregrid.org",
     download_url = "TBD on svn",
     classifiers = [
-    "Development Status :: 1 - Alpha",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: Apache 2.0",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Topic :: Text Processing :: Markup"],
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache 2.0",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup"],
     keywords = "Grid, Cloud, HPC",
     package_dir = { '' : '.' },
     packages = [
@@ -32,6 +30,9 @@ futuregrid is a python library that contains the code related dynamically provis
     install_requires = [
         'setuptools',
         'cmd2'
+        ],
+    scripts = [
+        'fg-shell.py'
         ]
     )
 
