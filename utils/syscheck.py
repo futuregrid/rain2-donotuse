d4f95003915cec1750979daed446c564a1ca4623
Gregor von Laszewski <laszewski@gmail.com>
2011-08-01 19:21:35 +0000

    added a check for python version 2.7

--- utils/syscheck.py
+++ utils/syscheck.py
@@ -1,4 +1,5 @@
 '''@package futuregrid.utils
+checks some simple system parameters to see if we have enough to run the shell
 '''
 
 import sys
@@ -6,9 +7,11 @@ import sys
 class sysCheck():
 
     def __init__(self):
+        ''' initializes the system check class and conducts the checks'''
         self.checkPythonVersion()
 
     def checkPythonVersion(self):
+        '''checks if the python version is above or equal to 2.7'''
         print "SYSTEM INFO"
         print "==========="
         if (sys.version_info < (2,7) ):
