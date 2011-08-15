8b7e62550ff46bd4a862e1bb8d627f1d88dfa50a
Mike Lewis <mlewisaquarius@gmail.com>
2011-08-15 13:37:34 +0000

    Tested and made corrections, replaced return with exit commands

--- utils/distributions.py
+++ utils/distributions.py
@@ -34,9 +34,11 @@ class Distributions() :
             self.openssl_dir = 'openssl-1.0.0d'
             self.cherrypy_dist = 'http://download.cherrypy.org/cherrypy/3.2.0/CherryPy-3.2.0.tar.gz'
             self.cherrypy_dir = 'CherryPy-3.2.0'
+            self.pymongo_cmd = 'sudo python -m easy_install pymongo'
             cmdOutput = 'x86_64\n'#runCmd('uname -p')
         if cmdOutput == "i386\n" :
             self.mongo_dist = 'http://fastdl.mongodb.org/osx/mongodb-osx-i386-1.8.2.tgz'
+            self.pymongo_cmd = 'sudo python easy_install pymongo'
             self.mongo_dir = 'mongodb-osx-i386-1.8.2'
         elif cmdOutput == 'x86_64\n' :
             self._log.info('Using osx x86_64')
@@ -48,6 +50,7 @@ class Distributions() :
             self.openssl_dir = 'openssl-1.0.0d'
             self.cherrypy_dist = 'http://download.cherrypy.org/cherrypy/3.2.0/CherryPy-3.2.0.tar.gz'
             self.cherrypy_dir = 'CherryPy-3.2.0'
+            self.pymongo_cmd = 'sudo python easy_install pymongo'
             cmdOutput = 'x86_64\n' #runCmd('uname -p')
             if cmdOutput == "i386\n" :
                 self.mongo_dist = 'http://fastdl.mongodb.org/linux/mongodb-linux-i686-1.8.2.tgz'
@@ -59,7 +62,6 @@ class Distributions() :
     ## Execute shell based command
     # @param cmd the command string
     def runCmd(self, cmd, isShell = False):
-        print("Executing command " + cmd)
         if isShell == True :
             p = Popen(cmd.split(' '), stdout=PIPE, stderr=PIPE, shell = True)
         else :
@@ -71,10 +73,13 @@ class Distributions() :
             self._log.debug('stdout: '+std[0])
         
         if p.returncode != 0:
-            self._log.error('Command: '+cmd+' failed, status: '+str(p.returnCode)+' --- '+std[1])
+            self._log.error('Command: '+cmd+' failed, status: '+str(p.returncode)+' --- '+std[1])
 
         return std[0]
 
+    def checkopenssl(self) :
+        self.runCmd('which openssl')
+        exit(self.returnCode)
 
     ## Retrieve openssl distribution
     # Return code 0:  succesful installation,  return code 1:  unsuccessful
@@ -83,64 +88,69 @@ class Distributions() :
         # Retrieve distribution
         self.runCmd('wget ' + self.openssl_dist)
         if (self.returnCode != 0) :
-            return self.returnCode
+            exit(self.returnCode)
         self.runCmd('tar -xvf ' + self.openssl_dir + '.tar.gz')
         if (self.returnCode != 0) :
-            return self.returnCode        
+            exit(self.returnCode)
         cmd = 'mv -f ' + self.openssl_dir + ' ' + install_dir
-        return self.returnCode
+        self.runCmd(cmd)
+        exit(self.returnCode)
 
     ## Retrieve openssl distribution
     # Return code 0 - succesful installation, return code 1 unsucessful
     def getmongo(self, install_dir):
- #       install_dir = 'mongodb'
         self.runCmd('wget ' + self.mongo_dist)
         if (self.returnCode != 0) :
-            return self.returnCode
+            exit(self.returnCode)
         self.runCmd('tar -xvf ' + self.mongo_dir + '.tgz')
         if (self.returnCode != 0) :
-            return self.returnCode
-        cmd = 'mv -f ' + mongo_dir + ' ' + install_dir
-        self.runCmd(cmd)
-        if (self.returnCode != 0) :
-            return self.returnCode
-        try :
-            import pymongo
-        except ImportError:
-            # To use easy install on linux make sure you have the python-setuptools:   sudo apt-get install python-setuptools
-            self.runCmd('sudo easy_install pymongo')
-        return self.returnCode
+            exit(self.returnCode)
+        cmd = 'mv -f ' + self.mongo_dir + ' ' + install_dir
+        self.runCmd(cmd) 
+        exit(self.returnCode)
 
-    def checkmongo(self):
+    def checkpymongo(self) :
+        try :
+            import pymongo
+            exit(0)
+        except ImportError :
+            self._log.info("missing pymongo library")
+            exit(1)
+
+    def getpymongo(self):
         try :
             import pymongo
         except ImportError:
+            self.runCmd(self.pymongo_cmd)
+        
+        try :
+            import pymongo
+            self._Log.info("Able to install pymongo")
+        except ImportError :
             self._log.info("unable to install pymongo")
-            return 1
-        return 0
+            exit(1)
+
+        exit(0)
 
     ## Retrieve cherrypy distribution
     # Return code 0 - succesful installation, return code 1 unsuccessful
     def getcherrypy(self,install_dir):
-#        install_dir = 'cherrypy'
-        print("Install directory : " + install_dir)
         self._log.info("---RETRIEVING CHERRYPY DISTRIBUTION-----")
         self.runCmd('wget ' + self.cherrypy_dist)
         if (self.returnCode != 0) :
-            return self.returnCode
+            exit(self.returnCode)
         self.runCmd('tar -xvf ' + self.cherrypy_dir + '.tar.gz')
         cmd = 'mv -f ' + self.cherrypy_dir + ' ' + install_dir
         self.runCmd(cmd)
-        return self.returnCode
+        exit(self.returnCode)
 
     ## Check cherrypy
     def checkcherrypy(self):
         try :
             import cherrypy
         except ImportError:
-            print("unable to install cheryypy")
-            return 1
-        return 0
+            exit(1)
+        exit(0)
 
     ## Retrieve twill distribution
     # Return code 0 - successful installation, return code 1 unsuccessful
@@ -150,15 +160,15 @@ class Distributions() :
         except ImportError :
             self._log.info("--- RETRIEVING TWILL DISTRIBUTION ----")
             self.runCmd('sudo easy_install twill')
-        return self.returnCode
+        exit(self.returnCode)
 
     def checktwill(self) :
         try :
             import twill
         except ImportError:
             self._log.input("Unable to install twill")
-            return 1
-        return 0
+            exit(1)
+        exit(0)
 
     
         
