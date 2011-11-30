e709339f9f7246a2f81b712427fe3a8276643965
Javi Diaz <javier.diazmontes@gmail.com>
2011-11-30 15:23:38 +0000

    image management

--- image/management/IMDeploy.py
+++ image/management/IMDeploy.py
@@ -341,7 +341,7 @@ class IMDeploy(object):
             return msg
         imagelist = []
         for i in images:
-            imagelist.append(str(i).split(":")[1] + "  -  " + str(i.location))
+            imagelist.append(str(i).split(":")[1] + "  -  " + str(i.location) + " - " + str(i.status))
         
         return imagelist
         
