6c8bd07531d374e2252a6dbe0d07b3704fb89d07
Javi Diaz <javier.diazmontes@gmail.com>
2012-02-02 18:50:23 +0000

    image management

--- image/management/IMDeploy.py
+++ image/management/IMDeploy.py
@@ -20,6 +20,9 @@ import hashlib
 import time
 import boto.ec2
 import boto
+from boto.s3.connection import OrdinaryCallingFormat
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
 from random import randrange
 
 from IMClientConf import IMClientConf
